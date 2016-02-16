a5507eb0814cb0f479e42c797084df6a55c5dbc5
Cesar Villalobos <Cesarvh@Cesars-MacBook-Pro.local>
2016-02-16 00:52:13 -0800

    Test run

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
+var main = function() {
 confirm("What a loser");
 confirm("am i right or nah");
 confirm("whyd you even click this link");
-
 while true {
     alert("bruh");
 }
+}
+main()
+// ready(main);
